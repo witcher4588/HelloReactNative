adb248aa3113959b14273fe0953f761cc0cbcca0
witcher4588 <kev4588@gmail.com>
2020-11-19 20:48:15 -0600

    nueva actualizacion

--- App.js
+++ App.js
@@ -7,13 +7,10 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
+import {  
   StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
 } from 'react-native';
 
 import { Colors} from 'react-native/Libraries/NewAppScreen';
@@ -23,15 +20,35 @@ import { Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App: () => React$Node = () => {
   return (
-    <View>
-        <Text>Hola Mundo</Text>
+    <View style={styles.container}>
+        <Text style={styles.welcome}>Hola Mundo</Text>
+        <Text style={styles.welcome, {flex:1,textAlign:'center',}}>Hola Mundo</Text>
+        <Text style={styles.welcome}>Hola Mundo</Text>
+
+                        
       </View>
    
   );
 };
 
 const styles = StyleSheet.create({
- 
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    backgroundColor: '#5c089e',
+    //flexDirection: 'row',
+   
+    
+  },
+  welcome:{
+    flex: 2,
+    fontSize: 20,
+    textAlign: 'center',
+    backgroundColor: 'blue',
+    margin: 10,
+    
+    
+  },
 });
 
 export default App;
